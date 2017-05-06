97a08bb8da82678ec734d87ce7d8ef12c808a3da
Rozerin Köysu <rozerinkoysu@gmail.com>
2017-05-06 21:40:08 +0300

    infoviewmodel class has been created

--- app/src/main/java/volley/com/kodluyoruzvolley/viewmodel/InfoViewModel.java
+++ app/src/main/java/volley/com/kodluyoruzvolley/viewmodel/InfoViewModel.java
@@ -5,4 +5,33 @@ package volley.com.kodluyoruzvolley.viewmodel;
  */
 
 public class InfoViewModel {
+    private String name;
+    private int age;
+
+  //  @SerializedName("E-mail");
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
